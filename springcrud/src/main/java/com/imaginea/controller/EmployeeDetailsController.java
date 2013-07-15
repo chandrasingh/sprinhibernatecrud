b09c9a38375d921d651c15b75bc28da66dc78264
chandrasingh <chandra.singh@imaginea.com>
2013-07-15 12:35:41 +0530

    added the controller n tested the spring part seems to be working.

--- springcrud/src/main/java/com/imaginea/controller/EmployeeDetailsController.java
+++ springcrud/src/main/java/com/imaginea/controller/EmployeeDetailsController.java
@@ -1,5 +1,27 @@
 package com.imaginea.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.imaginea.services.EmployeeService;
+
+@Controller
 public class EmployeeDetailsController {
+	
+	@Autowired
+	EmployeeService employeeService;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(ModelMap model) {
+		System.out.println("in index");
+		model.addAttribute("message", "Its Working");
+		model.addAttribute("message2", "Its Really working.");
+		return "index";
+	}
 
 }
